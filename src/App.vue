1e176817ae50f6bc39f10d99ef92926ff74a26b4
upasadena <underscore.pasadena@gmail.com>
2023-08-04 23:41:11 +0000

    Add component switching functionality (finally)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,49 @@
 <template>
   <v-app>
+    <v-row
+      v-if="currentComponent == 0"
+      style="text-align: center;"
+    >
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="900px"
+      style="text-align: center;"
+    >
+      <v-card>
+        <v-card-item>
+          <v-card-title>Interactive CYOA Creator 2</v-card-title>
+          <v-card-subtitle>
+            A spiritual successor to MeanDelay's Creator.
+          </v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          <v-container>
+            <v-row
+              justify="center"
+            >
+              <!-- Buttons for the menu -->
+              <v-col
+                cols="auto"
+                v-for="button in menuButtons"
+                :key="button.id"
+              >
+                <v-btn
+                  small
+                  @click="currentComponent = button.id"
+                  style=""
+                >
+                  {{ button.text }}
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+    </v-row>
+
     <keep-alive>
       <component :is="components[currentComponent]"/>
     </keep-alive>
@@ -8,16 +52,22 @@
 
 <script setup lang="ts">
   import { onActivated, ref } from "vue";
-  import Home from "./views/Home.vue";
   import About from "./views/About.vue";
 
   const components = [
-    Home,
+    "Home", // Placeholder value
     About,
   ];
 
   // Default value is Home
   const currentComponent = ref(0);
+  const dialog = ref(true); // open dialog by default or not
+  const menuButtons = {
+    about: {
+      id: 1,
+      text: "About this Creator",
+    },
+  }
 
   // When the app is launched
   onActivated(() => {
--- src/router/index.ts
+++ src/router/index.ts
@@ -9,13 +9,13 @@ const routes = [
       // Home is the menu that is starts with
       {
         path: '',
-        name: 'Home',
+        name: 'App',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(
           /* webpackChunkName: "home" */
-          '@/views/Home.vue'
+          '@/App.vue'
         ),
       }
     ],
