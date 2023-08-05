22faa565638f41b5676ae226f76daf8563a581c1
upasadena <underscore.pasadena@gmail.com>
2023-08-05 00:57:22 +0000

    Add working back button

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,10 @@
 <template>
-  <v-btn icon="mdi-arrow-left"/>
+  <v-btn
+    icon="mdi-chevron-left"
+    @click="$emit('resetCurrentComponent')"
+  />
   <h1>Interactive CYOA Creator 2 Information</h1>
 </template>
 
 <script lang="ts" setup>
-
 </script>
